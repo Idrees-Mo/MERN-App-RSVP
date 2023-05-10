c8f626d24f34daa956f48430f8b00c1675d23cf8
idrees <muhammad.idrees@gecurrent.com>
2023-05-10 21:50:19 +1000

    updated class with className

--- client/src/components/guests/GuestForm.js
+++ client/src/components/guests/GuestForm.js
@@ -1,81 +1,120 @@
-import React, { useContext, useState, useEffect } from 'react'
-import GuestContext from '../../comtext/guestContext/guestContext'
+import React, { useContext, useState, useEffect } from 'react';
+import GuestContext from '../../comtext/guestContext/guestContext';
 
 const GuestForm = () => {
-  const context = useContext(GuestContext)
-  const { addGuest, editGuest, clearEdit, update_Guest } = context
+  const context = useContext(GuestContext);
+  const { addGuest, editGuest, clearEdit, update_Guest } = context;
 
   useEffect(() => {
     if (editGuest !== null) {
-      setGuest(editGuest)
+      setGuest(editGuest);
     } else {
       setGuest({
         name: '',
         phone: '',
         diet: 'Non-Veg'
-      })
+      });
     }
-  }, [editGuest, context])
+  }, [editGuest, context]);
 
   const [guest, setGuest] = useState({
     name: '',
     phone: '',
     diet: 'Non-Veg'
-  })
-  const { name, phone, diet } = guest
+  });
+  const { name, phone, diet } = guest;
   const onchange = (e) => {
     setGuest({
       ...guest,
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
   const onsubmit = (e) => {
     e.preventDefault();
     if (editGuest === null) {
       addGuest(guest);
-
     } else {
-      update_Guest(guest)
-      clearEdit()
+      update_Guest(guest);
+      clearEdit();
     }
     setGuest({
       name: '',
       phone: '',
-      diet: 'Non-Veg',
-    })
-  }
+      diet: 'Non-Veg'
+    });
+  };
   return (
-
     <div className="invite-section">
       <h1>{editGuest !== null ? 'Edit Guest' : 'Invite Someone'}</h1>
-      <form onSubmit={onsubmit} >
-        <input type="text" placeholder="Name" name="name" value={name} onChange={onchange} required />
-        <input type="text" placeholder="Phone" name="phone" value={phone} onChange={onchange} required />
-        <p className="options-label">
-          Dietary</p>
+      <form onSubmit={onsubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={name}
+          onChange={onchange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Phone"
+          name="phone"
+          value={phone}
+          onChange={onchange}
+          required
+        />
+        <p className="options-label">Dietary</p>
         <div className="options">
-          <label class="container">Non-Veg
-          <input type="radio" name="diet" value="Non-Veg" onChange={onchange} checked={diet === "Non-Veg"} />
-            <span class="checkmark"></span>
+          <label className="container">
+            Non-Veg
+            <input
+              type="radio"
+              name="diet"
+              value="Non-Veg"
+              onChange={onchange}
+              checked={diet === 'Non-Veg'}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label class="container">Vegan
-          <input type="radio" name="diet" value="Vegan" onChange={onchange} checked={diet === "Vegan"} />
-            <span class="checkmark"></span>
+          <label className="container">
+            Vegan
+            <input
+              type="radio"
+              name="diet"
+              value="Vegan"
+              onChange={onchange}
+              checked={diet === 'Vegan'}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label class="container">Pescatarian
-          <input type="radio" name="diet" value="Pescatarian" onChange={onchange} checked={diet === "Pescatarian"} />
-            <span class="checkmark"></span>
+          <label className="container">
+            Pescatarian
+            <input
+              type="radio"
+              name="diet"
+              value="Pescatarian"
+              onChange={onchange}
+              checked={diet === 'Pescatarian'}
+            />
+            <span className="checkmark"></span>
           </label>
         </div>
-        <input type="submit" value={editGuest !== null ? 'Update Guest' : 'Add Guest'} className="btn" />
-        {editGuest !== null ? < input onClick={clearEdit} type="button" className="btn clear" value="Cancel" /> : null}
+        <input
+          type="submit"
+          value={editGuest !== null ? 'Update Guest' : 'Add Guest'}
+          className="btn"
+        />
+        {editGuest !== null ? (
+          <input
+            onClick={clearEdit}
+            type="button"
+            className="btn clear"
+            value="Cancel"
+          />
+        ) : null}
       </form>
-
     </div>
-  )
-}
+  );
+};
 
-export default GuestForm
-
-
-
+export default GuestForm;
--- client/src/components/guests/GuestItem.js
+++ client/src/components/guests/GuestItem.js
@@ -1,41 +1,63 @@
-import React, { useContext } from 'react'
-import GuestContext from '../../comtext/guestContext/guestContext'
+import React, { useContext } from 'react';
+import GuestContext from '../../comtext/guestContext/guestContext';
 
 const GuestItem = ({ guest }) => {
-  const { removeGuest, edit_Guest, clearEdit, update_Guest } = useContext(GuestContext)
-  const { _id, name, phone, diet, isconfirmed } = guest
+  const { removeGuest, edit_Guest, clearEdit, update_Guest } =
+    useContext(GuestContext);
+  const { _id, name, phone, diet, isconfirmed } = guest;
 
   const handleRemove = () => {
-    removeGuest(_id)
-    clearEdit()
-  }
+    removeGuest(_id);
+    clearEdit();
+  };
   const onchange = () => {
-    update_Guest({ ...guest, isconfirmed: !isconfirmed })
-  }
+    update_Guest({ ...guest, isconfirmed: !isconfirmed });
+  };
 
   return (
-    <div class="guest-card">
-      <div class="card-head">
-        <div >
-          <label className={`${isconfirmed && 'confirm'}`}>Confirmed
-            <i className={`fas fa-check-square ${isconfirmed && 'confirm'}`}><input type="checkbox" onChange={onchange} /> </i>
+    <div className="guest-card">
+      <div className="card-head">
+        <div>
+          <label className={`${isconfirmed && 'confirm'}`}>
+            Confirmed
+            <i className={`fas fa-check-square ${isconfirmed && 'confirm'}`}>
+              <input type="checkbox" onChange={onchange} />{' '}
+            </i>
           </label>
         </div>
         <div>
-          <button title="Edit Guest"><i class="fas fa-user-edit" onClick={() => edit_Guest(guest)} ></i></button>
-          <button onClick={handleRemove} title="Remove Guest"><i class="fas fa-trash-alt remove"></i></button>
+          <button title="Edit Guest">
+            <i
+              className="fas fa-user-edit"
+              onClick={() => edit_Guest(guest)}
+            ></i>
+          </button>
+          <button onClick={handleRemove} title="Remove Guest">
+            <i className="fas fa-trash-alt remove"></i>
+          </button>
         </div>
       </div>
-      <div class="card-body">
+      <div className="card-body">
         <h2>{name}</h2>
-        <span class={'badge ' + (diet === 'Vegan' ? 'green' : diet === 'Non-Veg' ? 'red' : 'seaGreen')}>{diet}</span>
-        <div class="contact">
-          <i class="fas fa-phone-alt"></i>
+        <span
+          className={
+            'badge ' +
+            (diet === 'Vegan'
+              ? 'green'
+              : diet === 'Non-Veg'
+              ? 'red'
+              : 'seaGreen')
+          }
+        >
+          {diet}
+        </span>
+        <div className="contact">
+          <i className="fas fa-phone-alt"></i>
           <p>{phone}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GuestItem
+export default GuestItem;
