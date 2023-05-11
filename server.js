23f9eff9b13d20bc7713956b6d2dca77aebddf2b
idrees <muhammad.idrees@gecurrent.com>
2023-05-11 21:46:45 +1000

    no production

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.get('/', (req, res) => {
   });
 });
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
