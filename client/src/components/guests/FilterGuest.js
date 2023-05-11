e282db7a626031b50615c12f70617eeebad91a51
idrees <muhammad.idrees@gecurrent.com>
2023-05-11 10:39:35 +1000

    class to className

--- client/src/components/guests/FilterGuest.js
+++ client/src/components/guests/FilterGuest.js
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react'
-import GuestContext from '../../comtext/guestContext/guestContext'
+import React, { useContext } from 'react';
+import GuestContext from '../../comtext/guestContext/guestContext';
 
 const FilterGuest = () => {
-  const { toggleGuestFilter } = useContext(GuestContext)
+  const { toggleGuestFilter } = useContext(GuestContext);
   return (
     <div className="toggle">
-      <label class="switch">
+      <label className="switch">
         <input type="checkbox" onChange={() => toggleGuestFilter()} />
-        <span class="slider round"></span>
+        <span className="slider round"></span>
       </label>
       <p className="lead">Show attending only!</p>
     </div>
-  )
-}
-export default FilterGuest
+  );
+};
+export default FilterGuest;
