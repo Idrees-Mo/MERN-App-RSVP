209f59f7a7fa11ad4b71dd93b45fddacb422a525
idrees <muhammad.idrees@gecurrent.com>
2023-05-11 12:02:24 +1000

    add a message for default credentials

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,52 +1,87 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import AuthContext from '../../comtext/authContext/authContext'
+import AuthContext from '../../comtext/authContext/authContext';
 
 const Login = (props) => {
-  const { login, isAuthencated, error, clearErrors } = useContext(AuthContext)
+  const { login, isAuthencated, error, clearErrors } = useContext(AuthContext);
   useEffect(() => {
     if (isAuthencated) {
-      props.history.push('/')
-      clearErrors()
+      props.history.push('/');
+      clearErrors();
     } else {
-      clearErrors()
+      clearErrors();
     }
     // eslint-disable-next-line
-  }, [isAuthencated, props.history])
+  }, [isAuthencated, props.history]);
   const [user, setUser] = useState({
     email: '',
     password: ''
-  })
-  const { email, password } = user
+  });
+  const { email, password } = user;
 
-  const onchange = e => {
+  const onchange = (e) => {
     setUser({
       ...user,
       [e.target.name]: e.target.value
-    })
-    if (error !== null) { clearErrors() }
-  }
-  const onsubmit = e => {
-    e.preventDefault()
+    });
+    if (error !== null) {
+      clearErrors();
+    }
+  };
+  const onsubmit = (e) => {
+    e.preventDefault();
     login({
       email,
       password
-    })
-    clearErrors()
-  }
+    });
+    clearErrors();
+  };
   return (
     <div className="login">
       <h1>Login</h1>
       <form onSubmit={onsubmit}>
-        <input type="email" name="email" placeholder="Email" value={email} onChange={onchange} />
-        <input type="password" name="password" placeholder="Password" value={password} onChange={onchange} required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={onchange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={onchange}
+          required
+        />
         <input type="submit" value="Login" className="btn" />
       </form>
       <div className="question">
-        {error !== null && <button className="danger" type="button"  >{error} <span onClick={() => clearErrors()}>X</span></button>}
-        <p>Dont' have an accout? {" "} <Link to='/register'>Sign Up</Link></p>
+        {error !== null && (
+          <button className="danger" type="button">
+            {error} <span onClick={() => clearErrors()}>X</span>
+          </button>
+        )}
+        <p>
+          Dont' have an accout? <Link to="/register">Sign Up</Link>
+        </p>
+      </div>
+      <div>
+        <br />
+        <p>
+          Hi there, to test the app as a guest user, please use the following
+          credentials or signup, Thanks.
+        </p>
+        <br />
+        <h3>
+          Email: <span>test@test.com</span>
+        </h3>
+        <h3>
+          Password: <span>test123</span>
+        </h3>
       </div>
     </div>
-  )
-}
-export default Login
+  );
+};
+export default Login;
